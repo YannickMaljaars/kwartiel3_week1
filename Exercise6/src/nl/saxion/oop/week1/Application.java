1920bd719a9a886e221268a2518508f64a24472b
Yannick Maljaars <498523@student.saxion.nl>
2021-02-12 15:02:29 +0100

    Finished homework

--- Exercise6/src/nl/saxion/oop/week1/Application.java
+++ Exercise6/src/nl/saxion/oop/week1/Application.java
@@ -2,22 +2,25 @@ package nl.saxion.oop.week1;
 
 import nl.saxion.app.SaxionApp;
 
+import java.util.ArrayList;
+
 public class Application implements Runnable {
 	public static void main(String[] args) {
-		SaxionApp.start(new Application());
+		SaxionApp.start(new Application(),1000,600);
 	}
 
 	public void run() {
 
-		// TODO: Your implementation here
 
-		// We used the following code to produce the sample_output.png file
-//		BingoMill bm = new BingoMill();
-//
-//		SaxionApp.printLine("Drawing 10 random numbers using the Bingo Mill!");
-//		for (int i = 0; i < 10; i++) {
-//			SaxionApp.printLine("Draw " + i + ": " + bm.draw() + " (" + bm.getNumberOfBallsRemaining() + " balls remaining)");
-//		}
+//		 We used the following code to produce the sample_output.png file
+		BingoMill bm = new BingoMill();
+		bm.shuffle();
+
+		SaxionApp.printLine("Drawing 10 random numbers using the Bingo Mill!");
+		for (int i = 0; i < 10; i++) {
+			SaxionApp.printLine("Draw " + (i+1) + ": " + bm.draw() + " (" + bm.getNumberOfBallsRemaining() + " balls remaining)");
+		}
+
 
 	}
 }
--- Exercise6/src/nl/saxion/oop/week1/BingoMill.java
+++ Exercise6/src/nl/saxion/oop/week1/BingoMill.java
@@ -1,2 +1,51 @@
-package nl.saxion.oop.week1;public class BingoMill {
+package nl.saxion.oop.week1;
+
+
+import nl.saxion.app.SaxionApp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BingoMill {
+
+    private ArrayList<Integer> numbers = new ArrayList<>();
+
+    public BingoMill(){
+        for (int i = 0; i < 75; i++) {
+            numbers.add(i+1);
+        }
+    }
+
+    public ArrayList<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int draw() {
+        if (numbers.size() < 1) {
+            return -1;
+        } else {
+            return numbers.remove(0);
+        }
+    }
+
+    public int getNumberOfBallsRemaining() {
+        return numbers.size();
+    }
+
+    public void reset(){
+        numbers.clear();
+        for (int i = 0; i < 75; i++) {
+            numbers.add(i+1);
+        }
+        shuffle();
+    }
+
+    public void shuffle(){
+        for (int i = 0; i < 500; i++) {
+            int a = SaxionApp.getRandomValueBetween(0,75);
+            int b = SaxionApp.getRandomValueBetween(0,75);
+            Collections.swap(numbers, a, b);
+        }
+
+    }
 }
